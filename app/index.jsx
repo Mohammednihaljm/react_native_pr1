9d34505893719fa4f7ba04f81cc93c7df0fe7838
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-09-14 23:56:03 +0530

    button

--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,14 @@ import { StyleSheet, Text, View,Button } from 'react-native'
 import React from 'react'
 
 const index = () => {
+  const handleClick=()=>{
+    console.log("waiting")
+  }
   return (
     <View>
       <Text style={styles.text}>this first program</Text>
       <Text style={{fontSize:30}}>this first program</Text>
-      <Button title='CLICK'/>
+      <Button title='PAY NOW' onPress={handleClick} />
     </View>
   )
 }
@@ -15,6 +18,6 @@ export default index
 
 const styles = StyleSheet.create({
   text:{
-    fontSize:40,
+    fontSize:50,
   }
 })
