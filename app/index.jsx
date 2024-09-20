9e9e27754892e9b2976bac941f8057c9278e5d3e
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-09-20 23:38:57 +0530

    state

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,34 @@
 import { StyleSheet, Text, View,Button } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Profile } from '../components/Profile'
 import Card from '../components/Card'
 const index = () => {
 
-  const handleClick=()=>{
-    console.log("clicked")
+  const[name,setState]=useState('nihal')
+
+  const clickMe=()=>{
+
+    setState('mohammed nihal jm')
+    console.warn(`${name}`)
+
   }
-  const onClick=(name,age)=>{
-    console.warn(name,age)
-  }
+
+  // const handleClick=()=>{
+  //   console.log("clicked")
+  // }
+  // const onClick=(name,age)=>{
+  //   console.warn(name,age)
+  // }
   return (
     <View>
-      <Text style={styles.text}>this first program</Text>
+      <Text>MY NAME IS:{name}</Text>
+      <Button title='click' onPress={clickMe}/>
+      {/* <Text style={styles.text}>this first program</Text>
       <Text style={{fontSize:30}}>this first program</Text>
       <Button title='PAY NOW' onPress={handleClick} />
       <Profile/>
       <Card/>
-      <Button title='click' color={"black"} onPress={()=>onClick('nihal',30)}/>
+      <Button title='click' color={"black"} onPress={()=>onClick('nihal',30)}/> */}
     </View>
   )
 }
