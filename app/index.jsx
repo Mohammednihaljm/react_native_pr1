33a314a7f2121d9df9635de99fc05fda8a7f28b5
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-09-28 00:48:46 +0530

    textinput

--- app/index.jsx
+++ app/index.jsx
@@ -1,69 +1,27 @@
-import { StyleSheet, Text, View,Button } from 'react-native'
-import React, { useState } from 'react'
-import { Profile } from '../components/Profile'
-import Card from '../components/Card'
-import { externalStyle } from '../style'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { TextInput } from 'react-native'
+import { useState } from 'react'
+import { Button } from 'react-native'
+
 const index = () => {
-
-  const[name,setState]=useState('nihal')
-
-  const clickMe=()=>{
-
-    setState('mohammed nihal jm')
-    // console.warn(`${name}`)
-
+  const [name,setName]=useState("")
+  const handelInput=(val)=>{
+        setName(val)
   }
-
-  // const handleClick=()=>{
-  //   console.log("clicked")
-  // }
-  // const onClick=(name,age)=>{
-  //   console.warn(name,age)
-  // }
+  console.log(name)
+  const onChange=()=>{
+    setName("")
+  }
   return (
     <View>
-      <Text style={{color:"pink",fontSize:25,backgroundColor:"black"}}>MY NAME IS:{name}</Text>
-      <Button title='click' onPress={clickMe}/>
-      {/* <Text style={styles.text}>this first program</Text>
-      <Text style={{fontSize:30}}>this first program</Text>
-      <Button title='PAY NOW' onPress={handleClick} />
-      <Profile/>
-      <Card/>
-      <Button title='click' color={"black"} onPress={()=>onClick('nihal',30)}/> */}
-      <Child name={name} age={25}/>
+      <Text>hi</Text>
+      <TextInput placeholder='enter your name' style={{color:"black",borderWidth:1,backgroundColor:"white"}} value={name} onChangeText={handelInput}/>
+      <Button title='CLEAR'onPress={onChange}/>
     </View>
   )
 }
-
-const Child=({name,age})=>{
-  console.warn(name,age)
-  return(
-    <View>
-      <Text style={styles.heading}>I AM CHILD CODER:{name}</Text>
-      <Text style={styles.heading}>I AM CHILD CODER:{name}</Text>
-      <Text style={styles.heading}>I AM CHILD CODER:{name}</Text>
-      <Text style={styles.heading}>I AM CHILD CODER:{name}</Text>
-      <Text style={styles.heading}>I AM CHILD CODER:{name}</Text>
-      <Text style={externalStyle.heading}>my age:{age}</Text>
-    </View>
-  )
-}
-
 
 export default index
 
-
-
-
-const styles = StyleSheet.create({
-  heading:{
-    color:"red",
-    backgroundColor:"black",
-    fontSize:30,
-    fontWeight:"500",
-    textAlign:"center",
-    padding:10,
-    margin:10,
-    borderRadius:10,
-  }
-})
+const styles = StyleSheet.create({})
