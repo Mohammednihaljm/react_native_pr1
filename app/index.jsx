30cd7c3101ba429dbc0a594c73dde749dac63a1c
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-10-01 23:58:09 +0530

    signup

--- app/index.jsx
+++ app/index.jsx
@@ -2,22 +2,26 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { TextInput } from 'react-native'
 import { Button } from 'react-native'
+import { useState } from 'react'
 
 const index = () => {
+  const[name,setName]=useState("")
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
   return (
     <View>
       <Text>sign up</Text>
       <View>
-        <Text>NAME:</Text>
-        <TextInput placeholder='name' style={styles.input} />
+        <Text>NAME:{name}</Text>
+        <TextInput placeholder='name' style={styles.input} onChangeText={(val)=>{setName(val)}}/>
       </View>
       <View>
-        <Text>Email:</Text>
-        <TextInput placeholder='email' style={styles.input} />
+        <Text>Email:{email}</Text>
+        <TextInput placeholder='email' style={styles.input} onChangeText={(val)=>{setEmail(val)}} keyboardType='emai-address'/>
       </View>
       <View>
-        <Text>password:</Text>
-        <TextInput placeholder='password' style={styles.input} />
+        <Text>password:{password}</Text>
+        <TextInput placeholder='password' style={styles.input} onChangeText={(val)=>{setPassword(val)}} secureTextEntry={true}/>
       </View>
       <Button title='click me'/>
     </View>
