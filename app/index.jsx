2c8a9bbabfc9af74ac1149e45ebb72e2a032d74d
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-10-01 23:45:52 +0530

    signupform

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { TextInput } from 'react-native'
-import { useState } from 'react'
 import { Button } from 'react-native'
 
 const index = () => {
-  const [name,setName]=useState("")
-  const handelInput=(val)=>{
-        setName(val)
-  }
-  console.log(name)
-  const onChange=()=>{
-    setName("")
-  }
   return (
     <View>
-      <Text>hi</Text>
-      <TextInput placeholder='enter your name' style={{color:"black",borderWidth:1,backgroundColor:"white"}} value={name} onChangeText={handelInput}/>
-      <Button title='CLEAR'onPress={onChange}/>
+      <Text>sign up</Text>
+      <View>
+        <Text>NAME:</Text>
+        <TextInput placeholder='name' style={styles.input} />
+      </View>
+      <View>
+        <Text>Email:</Text>
+        <TextInput placeholder='email' style={styles.input} />
+      </View>
+      <View>
+        <Text>password:</Text>
+        <TextInput placeholder='password' style={styles.input} />
+      </View>
+      <Button title='click me'/>
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input:{
+    borderWidth:1,
+    padding:10,
+    fontSize:12,
+  }
+})
