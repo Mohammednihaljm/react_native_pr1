5dbcb2b6bbb5aa6eecd62d70accd3d340425b055
Mohammednihaljm <mohammednihaljm@gmail.com>
2024-10-09 08:09:39 +0530

    map.function

--- app/index.jsx
+++ app/index.jsx
@@ -1,50 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { FlatList } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-const DATA=[
-  {id:"1",title:'alfam'},
-  {id:"2",title:'mandhi'},
-  {id:"3",title:'biriyan'},
-  {id:"4",title:'rice'},
-  {id:"5",title:'porotta'},
-  {id:"6",title:'chicken curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-  {id:"7",title:'beef curry'},
-]
+const DATA = [
+  { id: "1", title: "alfam" },
+  { id: "2", title: "mandhi" },
+  { id: "3", title: "biriyan" },
+  { id: "4", title: "rice" },
+  { id: "5", title: "porotta" },
+  { id: "6", title: "chicken curry" },
+  { id: "7", title: "beef curry" },
+  { id: "8", title: "beef 1" },
+  { id: "9", title: "beef 2" },
+  { id: "10", title: "beef 3" },
+  { id: "11", title: "4" },
+  { id: "12", title: "5" },
+  { id: "13", title: "6" },
+  { id: "14", title: "7" },
+];
 
 const index = () => {
-  const renderItem=({item})=>(
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.id}+{item.title}</Text>
-    </View>
-  )
   return (
-    <FlatList data={DATA} renderItem={renderItem} keyExtractor={item=>item.id}/>
-    
-  )
-}
+    <ScrollView>
+    <View>
+      {DATA.map((item) => {
+        return (
+          <View>
+            <Text style={styles.item}>{item.title}</Text>
+          </View>
+        );
+      })}
+    </View>
+    </ScrollView>
+  );
+};
 
-export default index
+export default index;
 
 const styles = StyleSheet.create({
-  item:{
-    padding:20,
-    borderWidth:1,
-    color:'black',
-    borderBottomColor:"#ccc"
+  item: {
+    padding: 20,
+    borderWidth: 1,
+    color: "black",
+    borderBottomColor: "#ccc",
   },
-  title:{
-    fontSize:18,
+  title: {
+    fontSize: 18,
   },
-})
+});
